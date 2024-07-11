345f03d5b0b4a450c9ada97dd6265c1210b17250
sarunasbend <s.b.seriouscontact@gmail.com>
2024-07-11 20:42:37 +0100

    Fixed the painting component of the program.

--- GUI.java
+++ GUI.java
@@ -8,32 +8,58 @@ import java.awt.event.MouseListener;
 import javax.swing.*;
 
 public class GUI extends JFrame{
-    private DrawingGrid grid;
+    private Grid grid;
     private JPanel pixelGrid;
-    private JButton colourSelector;
-    private Color currentColor;
+    private JButton redButton;
     private int red;
+    private JButton greenButton;
     private int green;
+    private JButton blueButton;
     private int blue;
+    private Color currentColor;
 
     public GUI(){
         setSize(new Dimension(1000, 1000));
-        this.grid = new DrawingGrid(100, 100, 20, 20, 20);
-        this.pixelGrid = this.grid.getDrawingGridPanel();
-        /*this.colourSelector = new JButton("COLOUR");
-        this.colourSelector.setBounds(0, 400, 100, 100);
-        this.colourSelector.addMouseListener(new MouseAdapter() {
+        this.grid = new Grid(100, 100, 20, 20, 20);
+        this.pixelGrid = this.grid.getGridPanel();
+        this.redButton = new JButton("COLOUR");
+        this.redButton.setBounds(0, 400, 100, 100);
+        this.redButton.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent event) {
-                currentColor = new Color(red, green, blue);
                 red+=20;
-                green+=20;
-                blue+=20;
-                grid.setTileColor(currentColor);
+                if (red > 255){red = 0;}
+                currentColor = new Color(red, green , blue); 
+                grid.setPixelColor(currentColor);
             }
-        });*/
+        });
+
+        this.blueButton = new JButton("COLOUR");
+        this.blueButton.setBounds(100, 400, 100, 100);
+        this.blueButton.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event) {
+                blue+=20;
+                if (blue > 255){blue = 0;}
+                currentColor = new Color(red, green , blue); 
+                grid.setPixelColor(currentColor);
+            }
+        });
+
+        this.greenButton = new JButton("COLOUR");
+        this.greenButton.setBounds(200, 400, 100, 100);
+        this.greenButton.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event) {
+                green+=20;
+                if (green > 255){green = 0;}
+                currentColor = new Color(red, green , blue); 
+                grid.setPixelColor(currentColor);
+            }
+        });
+        
 
         setLayout(null);
-        //add(colourSelector);
+        add(redButton);
+        add(blueButton);
+        add(greenButton);
         add(pixelGrid);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
---
+++ Grid.java
@@ -0,0 +1,116 @@
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Grid {
+    public class Pixel{
+        private Color pixelColour;
+        private Rectangle rectangle;
+
+        public Pixel(Color pixeColor, int x, int y, int width, int height){
+            this.pixelColour = pixeColor;
+            this.rectangle = new Rectangle(x, y, width, height);
+        }
+        public Color getPixelColour(){return this.pixelColour;}
+        public Rectangle getRectangle(){return this.rectangle;}
+    }
+
+    private JPanel pixelGrid;
+    private int xPanel;
+    private int yPanel;
+
+    private int pixelSize;
+    private int gridWidth;
+    private int gridHeight;
+
+    private int mouseX = -1;
+    private int mouseY = -1;
+    private boolean isMouseHeldDown = false;
+
+    private final RenderingHints renderingHints  = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //anti-aliasing
+
+    private ArrayList<Pixel> pixels;
+    private Color currentColour = new Color(0, 0, 0);
+
+    public Grid(int xPanel, int yPanel, int pixelSize, int gridWidth, int gridHeight){
+        this.xPanel = xPanel;
+        this.yPanel = yPanel;
+        this.pixelSize = pixelSize;
+        this.gridWidth = gridWidth;
+        this.gridHeight = gridHeight;
+        this.pixels = new ArrayList<>();
+        createPixelGrid();
+    }
+
+    private void createPixelGrid(){
+        this.pixelGrid = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g){
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                drawGrid(g2d);
+                g2d.setClip(xPanel, yPanel, gridWidth * pixelSize, gridHeight * pixelSize); //might have to change to specify bounds
+                if ((mouseX >= 0) && (mouseY >= 0)){
+                    for (Pixel temp : pixels){
+                        Rectangle pixel = temp.getRectangle();
+                        g2d.setColor(temp.getPixelColour());
+                        g2d.drawRect(pixel.x, pixel.y, pixel.width, pixel.height);
+                        g2d.fillRect(pixel.x, pixel.y, pixel.width, pixel.height);
+                    }
+                }
+            }
+        };
+
+        this.pixelGrid.setSize(this.gridWidth * this.pixelSize, this.gridHeight * this.pixelSize);
+        this.pixelGrid.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent event){
+                isMouseHeldDown = true;
+                mouseX = ((event.getX() / pixelSize) * pixelSize);
+                mouseY = ((event.getY() / pixelSize) * pixelSize);
+                pixels.add(new Pixel(currentColour, mouseX, mouseY, pixelSize, pixelSize));
+                pixelGrid.repaint();
+            }
+            @Override
+            public void mouseReleased(MouseEvent event){
+                isMouseHeldDown = false;
+            }
+        });
+
+        this.pixelGrid.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent event){
+                if (isMouseHeldDown){
+                    mouseX = ((event.getX() / pixelSize) * pixelSize);
+                    mouseY = ((event.getY() / pixelSize) * pixelSize);
+                    pixels.add(new Pixel(currentColour, mouseX, mouseY, pixelSize, pixelSize));
+                    pixelGrid.repaint();
+                }
+            }            
+        });
+    }
+
+    private void drawGrid(Graphics2D g2d){
+        for (int i = xPanel; i < (gridWidth * pixelSize) + xPanel; i+=pixelSize){
+            g2d.drawLine(i, yPanel, i, yPanel + (gridHeight * pixelSize));
+        }
+
+        for (int j = yPanel; j < (gridHeight * pixelSize) + yPanel; j+=pixelSize){
+            g2d.drawLine(xPanel, j, xPanel + (gridWidth * pixelSize), j);
+        }
+    }
+
+    public JPanel getGridPanel(){return this.pixelGrid;}
+
+    public void setPixelColor(Color pixeColor){this.currentColour = pixeColor;}
+}
